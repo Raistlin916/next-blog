7d86c03719070c5d66e4b3ef3bd11fb97ea3a6ce
raistlin916 <raistlin916@gmail.com>
2019-03-25 15:03:16 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,48 @@
 import App, { Container } from 'next/app'
 import Head from 'next/head'
 import getConfig from 'next/config'
-import dynamic from 'next/dynamic'
 import React from 'react'
-import shiba from './test-1-2/shiba.png'
+import path from 'path'
+
+function importAll(r) {
+  let images = {}
+  r.keys().map(key => {
+    images[key.replace(/^\./, '')] = r(key)
+  })
+  return images
+}
+const images = importAll(require.context('./', true, /\.(png|jpe?g|svg)$/))
 
 export default class BlogApp extends App {
+  static async getInitialProps({ Component, ctx, router }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+    return { pageProps, pathname: router.pathname }
+  }
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps, pathname } = this.props
     const { publicRuntimeConfig } = getConfig()
     const { blogTitle } = publicRuntimeConfig
+
     return (
       <Container>
         <Head>
           <title>{blogTitle}</title>
         </Head>
-        <Component {...pageProps} components={{ img: props => {
-          return <img {...props} src={shiba} />
-        }}} />
+        <Component
+          {...pageProps}
+          components={{
+            img: props => {
+              const src = images[path.join(pathname, props.src).replace(/\\/g, '/')]
+              return (
+                <img {...props} src={src} />
+              )
+            }
+          }}
+        />
       </Container>
     )
   }
